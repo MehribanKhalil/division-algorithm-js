ce3b6af5e6be418841a97d074f69e7a03acbd0a9
Mehriban Khalilova <tu833KMN0@code.edu.az>
2023-10-11 10:34:16 +0400

    task finish

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+
+let a=42;
+
+
+if(a%21===0){
+    console.log(`${a} ededi hem 3e hem de 7e bolunur`)
+
+} else if(a%7===0){
+    console.log(`${a} ededi  7e bolunur`)
+
+} else if(a%3===0){
+    console.log(`${a} ededi 3e bolunur`)
+
+} else{
+    console.log(`${a} ededi ne 3e ne de 7e  bolunmur`)
+
+}
